beafd90ccdf4909e2ddc91ef7736ba50961ca4d8
Eric Park <epark0995@gmail.com>
2020-07-23 10:58:39 -0700

    favorites

---
+++ script.js
@@ -0,0 +1,6 @@
+let favoriteGame = ['League of Legends', 'Bloodborne', 'Zelda', 'Monster Hunter', 'Crash Bandicoot', 'Pokemon', 'Maplestory', 'God of War', 'Super Smash', 'Warframe']
+
+
+for (i = 1; i <= 10; i++) {
+    document.getElementById("1").innerHTML
+}
